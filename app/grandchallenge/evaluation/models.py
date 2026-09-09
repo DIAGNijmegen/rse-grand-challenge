118f52b547fcd53c84bea9009c9d57d3142c05db
Anne Mickan <30069334+amickan@users.noreply.github.com>
2026-09-09 10:13:10 +0200

    Also move the duration fields and input_prefixes out of the base class

--- app/grandchallenge/evaluation/models.py
+++ app/grandchallenge/evaluation/models.py
@@ -2179,6 +2179,44 @@ class Evaluation(CIVForObjectMixin, ComponentJob):
         related_name="%(app_label)s_%(class)ss_as_output",
     )
 
+    exec_duration = models.DurationField(
+        null=True,
+        default=None,
+        editable=False,
+        help_text=(
+            "The duration of the execution, if measured. "
+            "Excludes data validation, container pulling, model downloading, "
+            "data downloading and data uploading times. "
+            "Includes model loading time, input data loading time, "
+            "processing time, output data writing time and "
+            "any delays from shared hardware issues."
+        ),
+    )
+    invoke_duration = models.DurationField(
+        null=True,
+        default=None,
+        editable=False,
+        help_text=(
+            "The duration of the invocation, if measured. "
+            "Excludes data validation, container pulling, model downloading, "
+            "data downloading and data uploading times. "
+            "Potentially excludes model loading time, depending on the "
+            "users implementation. "
+            "Includes input data loading time, "
+            "processing time, output data writing time and "
+            "any delays from shared hardware issues."
+        ),
+    )
+    input_prefixes = models.JSONField(
+        default=dict,
+        editable=False,
+        help_text=(
+            "Map of the ComponentInterfaceValue id to the path prefix to use "
+            "for this input, e.g. {'1': 'foo/bar/'} will place CIV 1 at "
+            "/input/foo/bar/<relative_path>"
+        ),
+    )
+
     published = models.BooleanField(default=True, db_index=True)
     rank = models.PositiveIntegerField(
         default=0,
