f87bfc7f4265f00271a6e84f1ece9b89dc16e1ec
Chris van Run <chris.vanrun@radboudumc.nl>
2026-09-18 16:00:11 +0200

    Inline var.

--- app/grandchallenge/contact/emails.py
+++ app/grandchallenge/contact/emails.py
@@ -1,13 +1,11 @@
 from django.core.mail import mail_managers
 from django.utils.html import format_html
 
-CONTACT_SUBJECT_PREFIX = "[CONTACT-US] "
-
 
 def send_contact_email(*, email, message):
     """Send a contact form submission to the site managers."""
 
-    subject = f"{CONTACT_SUBJECT_PREFIX}{email}"
+    subject = f"[CONTACT-US] {email}"
 
     body = format_html(
         "Email: {email}\n\nMessage:\n{message}\n",
