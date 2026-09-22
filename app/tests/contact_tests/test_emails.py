2e19ff5b15713a1d5593f2b7183424c6fc1ff056
Chris van Run <chris.vanrun@radboudumc.nl>
2026-09-22 10:37:58 +0200

    Use EmailMessage directly

--- app/tests/contact_tests/test_emails.py
+++ app/tests/contact_tests/test_emails.py
@@ -7,7 +7,6 @@ from tests.utils import get_view_for_user
 @pytest.mark.django_db
 def test_valid_submission_sends_email(client, settings):
     settings.MANAGERS = [("Manager", "manager@example.org")]
-    settings.EMAIL_SUBJECT_PREFIX = ""
 
     response = get_view_for_user(
         client=client,
@@ -25,8 +24,9 @@ def test_valid_submission_sends_email(client, settings):
 
     assert len(mail.outbox) == 1
     email = mail.outbox[0]
-    assert email.subject.startswith("[CONTACT-US] ")
+    assert email.subject == "[testserver] New contact message"
     assert email.to == ["manager@example.org"]
+    assert email.reply_to == ["jane@example.org"]
 
     body = email.body
     assert "I have a question about your platform." in body
@@ -36,7 +36,6 @@ def test_valid_submission_sends_email(client, settings):
 @pytest.mark.django_db
 def test_valid_submission_shows_success_message(client, settings):
     settings.MANAGERS = [("Manager", "manager@example.org")]
-    settings.EMAIL_SUBJECT_PREFIX = ""
 
     response = get_view_for_user(
         client=client,
