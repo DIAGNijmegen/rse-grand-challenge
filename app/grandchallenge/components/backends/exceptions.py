5803d918caebde8abd56904ba1c6d1ba9ee7c62e
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2026-09-22 17:14:19 +0200

    Fix B042

--- app/grandchallenge/components/backends/exceptions.py
+++ app/grandchallenge/components/backends/exceptions.py
@@ -5,8 +5,8 @@ class ComponentBaseException(Exception):
 class ComponentException(ComponentBaseException):
     """These exceptions will be sent to the user"""
 
-    def __init__(self, message, message_details=None):
-        super().__init__(message, message_details)
+    def __init__(self, message, message_details=None):  # noqa: B042
+        super().__init__(message)
         self.message = message
         self.message_details = message_details
 
